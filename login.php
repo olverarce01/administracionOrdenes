bcfabde00fe22c2a297621358345c8479d392c34
NN <mauricio.mamani.chambi@alumnos.uta.cl>
2022-10-03 19:12:40 -0300

    cambios pequeños
    -carpeta iimagenes

---
+++ login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.9.1/chart.min.js" integrity="sha512-ElRFoEQdI5Ht6kZvyzXhYG9NqjtkmlkfYk0wr6wHxU9JEHakS7UJZNeml5ALk+8IKlU6jDgMabC3vkumRokgJA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    
+    <link Type="text/css" rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" rel="stylesheet" href="./css/estilos.css">
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
+    
+    <title>Login</title>
+</head>
+<body>
+<header>
+<nav class="navbar navbar-light bg-light nav-color">
+    <!-- Logo UTA -->
+    <a class="navbar-brand" href="#">
+      <img src="./img/utalogo.png" width="300" height="80" class="d-inline-block align-top" alt="Logo Uta">
+    </a>
+  </nav>
+</header>
+<main>
+
+</main>
+<footer>
+
+</footer>
+</body>
+</html>
